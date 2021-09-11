6fa869966534e0e64933c540e50b5add8115766f
Ben Gottlieb <ben@standalone.com>
2021-09-11 05:07:33 -0500

    Initial Commit

---
+++ ConveyTest/ConveyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConveyTestApp.swift
+//  ConveyTest
+//
+//  Created by Ben Gottlieb on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ConveyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
