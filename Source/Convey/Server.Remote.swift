7bb1cff893ca81395f62842ebcec18246b4673e4
Ben Gottlieb <ben@standalone.com>
2021-09-12 17:25:40 -0500

    Server.Remote is now hashable and Identifiable

--- Source/Convey/Server.Remote.swift
+++ Source/Convey/Server.Remote.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 public extension Server {
-	struct Remote {
+	struct Remote: Identifiable, Hashable {
 		public let name: String?
 		public let url: URL
 		public let shortName: String?
-		
+		public var id: URL { url }
+		public func hash(into hasher: inout Hasher) {
+			hasher.combine(url)
+		}
+
 		public init(_ url: URL, name: String? = nil, shortName: String? = nil) {
 			self.name = name
 			self.shortName = shortName
