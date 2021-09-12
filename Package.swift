75ced37f5f115db0a26222d759eb9155ee2b3b3a
Ben Gottlieb <ben@standalone.com>
2021-09-12 16:44:37 -0500

    updated minimum suite requirements

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 				targets: ["Convey"]),
 	 ],
 	 dependencies: [
-		.package(url: "https://github.com/bengottlieb/Suite.git", from: "0.10.92"),
+		.package(url: "https://github.com/bengottlieb/Suite.git", from: "0.10.93"),
 	 ],
 	 targets: [
 		  // Targets are the basic building blocks of a package. A target can define a module or a test suite.
