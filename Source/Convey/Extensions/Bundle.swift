b1f69c8347dd9c1f0bb2b17252fb27135252f175
Ben Gottlieb <ben@standalone.com>
2022-05-18 10:09:19 -0500

    updated the Bundle extension

--- Source/Convey/Extensions/Bundle.swift
+++ Source/Convey/Extensions/Bundle.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 extension Bundle {
-   public var version: String { return self.infoDictionary?["CFBundleShortVersionString"] as? String ?? "" }
-   public var buildNumber: String { return self.infoDictionary?["CFBundleVersion"] as? String ?? "" }
-   public var name: String { return self.infoDictionary?["CFBundleName"] as? String ?? "" }
-   public var copyright: String { return self.infoDictionary?["NSHumanReadableCopyright"] as? String ?? "" }
+   var version: String { return self.infoDictionary?["CFBundleShortVersionString"] as? String ?? "" }
+   var buildNumber: String { return self.infoDictionary?["CFBundleVersion"] as? String ?? "" }
+   var name: String { return self.infoDictionary?["CFBundleName"] as? String ?? "" }
+   var copyright: String { return self.infoDictionary?["NSHumanReadableCopyright"] as? String ?? "" }
 }
