3b9abe85e9dbe84809ae08fe2430fec2b89ff047
Ben Gottlieb <ben@standalone.com>
2022-10-13 04:32:44 -0500

    added QueryItem extension

---
+++ Source/Convey/Extensions/URLQueryItem.swift
@@ -0,0 +1,26 @@
+//
+//  URLQueryItem.swift
+//  
+//
+//  Created by Ben Gottlieb on 10/9/22.
+//
+
+import Foundation
+
+public extension Array where Element == URLQueryItem {
+	func contains(name: String) -> Bool {
+		contains { $0.name == name }
+	}
+	
+	mutating func replace(name: String, with value: String) {
+		if let index = firstIndex(where: { $0.name == name }) {
+			self[index] = URLQueryItem(name: name, value: value)
+		} else {
+			self.append(name: name, value: value)
+		}
+	}
+	
+	mutating func append(name: String, value: String) {
+		self.append(URLQueryItem(name: name, value: value))
+	}
+}
