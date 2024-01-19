e95e512b6047a4c6726f38eb892c06a15dbc2796
Ben Gottlieb <ben@standalone.com>
2024-01-19 05:44:04 -0600

    added more options to PingButton

--- Sources/Convey/Views/PingButton.swift
+++ Sources/Convey/Views/PingButton.swift
@@ -6,26 +6,34 @@
 //
 
 import SwiftUI
-
+import Combine
 
 @available(iOS 14.0, macOS 12, watchOS 8, *)
-public struct PingButton<TaskKind: ServerTask>: View {
+public struct PingButton<TaskKind: ServerTask, Content: View>: View {
 	let target: TaskKind
 	@State private var lastDuration: TimeInterval?
 	@State private var error: Error?
 	@State private var isRunning = false
+	let frequency: TimeInterval?
+	let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+	@ViewBuilder var contentBuilder: (AnyView) -> Content
+	@Environment(\.scenePhase) var scenePhase
 	
 	var color: Color {
 		if lastDuration != nil { return .green }
 		if error != nil { return .red }
-		return .yellow
+		return .clear
 	}
 	
-	public init(_ target: TaskKind) {
+	public init(_ target: TaskKind, frequency: TimeInterval? = nil, @ViewBuilder content: @escaping (AnyView) -> Content) {
 		self.target = target
+		self.frequency = frequency
+		timer = Timer.publish(every: frequency ?? 10000000, on: .main, in: .common).autoconnect()
+		contentBuilder = content
 	}
 	
 	func ping() {
+		if isRunning { return }
 		isRunning = true
 		Task {
 			do {
@@ -38,16 +46,42 @@ public struct PingButton<TaskKind: ServerTask>: View {
 		}
 	}
 	
+	@ViewBuilder var indicator: some View {
+		if isRunning {
+			ProgressView()
+		} else {
+			ZStack {
+				Circle()
+					.fill(color)
+				Circle()
+					.stroke(.black)
+			}
+			.frame(width: 20, height: 20)
+		}
+	}
+	
 	public var body: some View {
-		Group {
-			Button(action: { ping() }) {
-				if isRunning {
-					ProgressView()
-				} else {
-					Circle()
-						.fill(color)
-						.frame(width: 20, height: 20)
-				}
+		Button(action: { ping() }) {
+			contentBuilder(AnyView(indicator))
+		}
+		.onReceive(timer) { _ in
+			ping()
+		}
+		.onChange(of: frequency) { newFreq in if newFreq != frequency, newFreq != nil { ping() }}
+		.onAppear { if frequency != nil { ping() }}
+		.onChange(of: scenePhase) { newPhase in
+			if frequency != nil, newPhase == .active { ping() }
+		}
+	}
+}
+
+@available(iOS 14.0, macOS 12, watchOS 8, *)
+extension PingButton where Content == HStack<TupleView<(Text, AnyView)>> {
+	public init(_ target: TaskKind, title: String, frequency: TimeInterval? = nil) {
+		self.init(target, frequency: frequency) { indicator in
+			HStack {
+				Text(title)
+				indicator
 			}
 		}
 	}
