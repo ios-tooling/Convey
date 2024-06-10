f5d23373da84e2d834b50eefbb9bf4b3a6a9b5f5
Ben Gottlieb <ben@standalone.com>
2024-06-09 21:36:03 -0500

    added TaskPath screen

---
+++ Sources/Convey/Views/TaskPathScreen.swift
@@ -0,0 +1,23 @@
+//
+//  TaskPathScreen.swift
+//
+//
+//  Created by Ben Gottlieb on 6/8/24.
+//
+
+import SwiftUI
+
+public struct TaskPathScreen: View {
+	public init() {
+		
+	}
+	
+	public var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+	}
+}
+
+#Preview {
+	TaskPathScreen()
+}
