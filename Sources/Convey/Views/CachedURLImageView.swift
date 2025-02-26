3f6d545511b3440495743bff5e4e291e8e831342
Ben Gottlieb <ben@standalone.com>
2025-02-25 18:39:33 -0600

    added Image.TemplateRenderingMode to CachedURLImage

--- Sources/Convey/Views/CachedURLImageView.swift
+++ Sources/Convey/Views/CachedURLImageView.swift
@@ -23,6 +23,7 @@ public struct CachedURLImage: View {
 	let kind: DataCache.CacheKind
 	let imageSize: ImageSize?
 	let deferredUntil: Date?
+	let renderingMode: Image.TemplateRenderingMode
 	
 	@State var cacheInfo: ImageCache.ImageInfo?
 	@State var cachedImage: PlatformImage?
@@ -36,7 +37,7 @@ public struct CachedURLImage: View {
 #endif
 	}
 	
-	public init(url: URL?, contentMode: ContentMode = .fit, placeholder: Image? = nil, showURLs: Bool = false, size: ImageSize? = nil, kind: DataCache.CacheKind = .default, error: Binding<Error?>? = nil, deferredUntil date: Date? = nil) {
+	public init(url: URL?, contentMode: ContentMode = .fit, placeholder: Image? = nil, showURLs: Bool = false, size: ImageSize? = nil, kind: DataCache.CacheKind = .default, error: Binding<Error?>? = nil, deferredUntil date: Date? = nil, renderingMode: Image.TemplateRenderingMode = .original) {
 		imageURL = url
 		_error = error ?? .constant(nil)
 		self.kind = kind
@@ -45,12 +46,13 @@ public struct CachedURLImage: View {
 		self.contentMode = contentMode
 		self.placeholder = placeholder
 		self.showURLs = showURLs
+		self.renderingMode = renderingMode
 
 		_cacheInfo = State(initialValue: ImageCache.instance.fetchLocalInfo(for: url, kind: kind, size: size))
 	}
 	
 	var imageView: Image? {
-		Image(platformImage: platformImage) ?? placeholder
+		Image(platformImage: platformImage)?.renderingMode(renderingMode) ?? placeholder?.renderingMode(renderingMode)
 	}
 	
 	var platformImage: PlatformImage? {
