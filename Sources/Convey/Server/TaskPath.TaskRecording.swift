74fe121819c44a6d28603c86f7b0e8c7125d6377
Ben Gottlieb <ben@standalone.com>
2025-06-13 19:07:37 -0500

    improved memory usage when loading a task path

--- Sources/Convey/Server/TaskPath.TaskRecording.swift
+++ Sources/Convey/Server/TaskPath.TaskRecording.swift
@@ -12,7 +12,7 @@ extension TaskPath {
 		public var id: URL { fileURL }
 		public let fileURL: URL
 		public let date: Date
-		public let payloadSize: Int64?
+		public var payloadSize: Int64?
 		public let duration: TimeInterval?
 		var url: URL?
 		public let fromDisk: Bool
@@ -26,17 +26,20 @@ extension TaskPath {
 			self.date = date
 			self.fromDisk = fromDisk
 			self.duration = duration
-			
-			if let raw = try? String(contentsOf: fileURL) {
-				let pieces = raw.components(separatedBy: RecordedTask.separator).filter { !$0.isEmpty }
-				url = URL(string: pieces.first?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")
-				if pieces.count > 2 {
-					payloadSize = Int64(pieces[pieces.count - 2].count)
+
+			autoreleasepool {
+				
+				if let raw = try? String(contentsOf: fileURL) {
+					let pieces = raw.components(separatedBy: RecordedTask.separator).filter { !$0.isEmpty }
+					url = URL(string: pieces.first?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")
+					if pieces.count > 2 {
+						payloadSize = Int64(pieces[pieces.count - 2].count)
+					} else {
+						payloadSize = nil
+					}
 				} else {
 					payloadSize = nil
 				}
-			} else {
-				payloadSize = nil
 			}
 		}
 	}
