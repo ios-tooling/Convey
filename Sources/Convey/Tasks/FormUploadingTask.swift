a41c8f4b83c56ebde8a1eb13e14871687d1bb0d3
Ben Gottlieb <ben@standalone.com>
2026-06-27 16:08:37 -0500

    made form URL encoding optional

--- Sources/Convey/Tasks/FormUploadingTask.swift
+++ Sources/Convey/Tasks/FormUploadingTask.swift
@@ -9,23 +9,36 @@ import Foundation
 
 public protocol FormUploadingTask: UploadingTask where UploadPayload == Data {
 	var formFields: [String: Sendable]? { get }
+	var shouldURLEncodePayload: Bool { get }
 }
 
 public extension FormUploadingTask {
 	var contentType: String? { "application/x-www-form-urlencoded" }
 	var uploadPayload: Data? { nil }
-	var uploadData: Data? { formFields?.formURLEncodedData }
+	var uploadData: Data? { formFields?.formURLEncodedData(shouldURLEncode: shouldURLEncodePayload) }
+	var shouldURLEncodePayload: Bool { false }
 }
 
 public extension [String: any Sendable] {
-	var formURLEncodedData: Data { formURLEncodedString.data(using: .utf8) ?? Data() }
+	func formURLEncodedData(shouldURLEncode: Bool) -> Data { formURLEncodedString(shouldURLEncode: shouldURLEncode).data(using: .utf8) ?? Data() }
 
-	var formURLEncodedString: String {
-		keys.sorted().compactMap { key in
-			guard let value = self[key] else { return nil }
-			return "\(key.formURLEncoded)=\("\(value)".formURLEncoded)"
+	func formURLEncodedString(shouldURLEncode: Bool) -> String {
+		if shouldURLEncode {
+			return keys.sorted().compactMap { key in
+				guard let value = self[key] else { return nil }
+				return "\(key.formURLEncoded)=\("\(value)".formURLEncoded)"
+			}
+			.joined(separator: "&")
+		} else {
+			var upload = ""
+			let sortedKeys = keys.sorted()
+			
+			for key in sortedKeys {
+				guard let value = self[key] else { continue }
+				upload += "\(key)=\(value)&"
+			}
+			return upload
 		}
-		.joined(separator: "&")
 	}
 }
 
